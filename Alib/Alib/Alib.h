ae47033c170d2d82862c067f403ed25abd6dd5ce
Dlu-git <1224595453@qq.com>
2020-07-27 13:43:01 +0800

    no message

---
+++ Alib/Alib/Alib.h
@@ -0,0 +1,9 @@
+#ifndef TestDll_H_
+#define TestDll_H_
+#ifdef MYLIBDLL
+#define MYLIBDLL extern "C" _declspec(dllimport) 
+#else
+#define MYLIBDLL extern "C" _declspec(dllexport) 
+#endif
+MYLIBDLL long double Add(float plus1, float plus2);
+#endif
