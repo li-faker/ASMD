b46969bc4e38fc8e68be868ccf2e0c23b16d640f
Dlu-git <1224595453@qq.com>
2020-07-27 13:48:24 +0800

    no message

---
+++ Cdll/Cdll/Cdll.cpp
@@ -0,0 +1,8 @@
+#include"Cdll.h"
+#include <iostream>
+using namespace std;
+ long double Mul(float plus1, float plus2)
+{
+long double Mul_result = plus1 * plus2;
+return Mul_result;
+}
---
+++ Cdll/UnitTest1/unittest1.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include"..\Cdll\Cdll.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: 在此输入测试代码
+			Assert::AreEqual(Mul(10,3),2.0,0.001);
+		}
+
+	};
+}
